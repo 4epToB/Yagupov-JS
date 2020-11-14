9f12864f795d4d15bc737740199e5ee2ce19f03e
4epToB <73242484+4epToB@users.noreply.github.com>
2020-11-14 17:15:51 +0300

    main

--- 2 dz/js/idex.js
+++ 2 dz/js/idex.js
@@ -1,10 +1,10 @@
 
 var monthNum=parseInt(prompt('Введите номер месяца', '1-12'));
-if(monthNum==1 || monthNum==2 || monthNum==3 ){
+if(monthNum==11 || monthNum==1 || monthNum==2 ){
     alert('Это зима')
-}else if(monthNum==4 || monthNum==5 || monthNum==6 ){
+}else if(monthNum==3 || monthNum==4 || monthNum==5 ){
     alert('Это весна')
-}else if(monthNum==7 || monthNum==8 || monthNum==9 ){
+}else if(monthNum==6 || monthNum==7 || monthNum==8 ){
     alert('Это лето')
 } else {
     alert('Это осень')
@@ -19,29 +19,37 @@ switch (unitName){
         alert(length+'м');
         break;
     case 2: 
-        length=length/1000
+        length=length*1000
         alert(length+'м')
         break
     case 3: 
         alert(length+'м')
         break
     case 4: 
-        length=length*1000
+        length=length/1000
         alert(length+'м')
         break
     case 5: 
-        length=length*100
+        length=length/100
         alert(length+'м')
         break
 }
 
 var chislo=parseInt(prompt('Введите целое число, лежащее в диапазоне от -999 до 999.', '-999 до 999'));
-if(chislo<0 && chislo>-100){
-    alert('отрицательное двузначное число')
+if(chislo<-9 && chislo>-100){
+    alert('Отрицательное двузначное число')
 }else if (chislo==0){
     alert('это ноль!')
 }else if (chislo>0 && chislo<10){
     alert('Положительное однозначное число')
+}else if (chislo<0 && chislo>-10){
+    alert('Отрицательное однозначное число')
+}else if (chislo>-1000 && chislo<-99){
+    alert('Отрицательное трёхзначное число')
+}else if (chislo>100 && chislo>9){
+    alert('Положительное двузначное число')
+}else if (chislo>1000 && chislo>99){
+    alert('Положительное трехзначное число')
 }
 
 var massive=1
@@ -59,7 +67,7 @@ var fary=parseInt(prompt('Введите количетсов Фейри,гр'))
 
 for (i=fary/0.5;i>0;i--){
     fary=fary-0.5
-    plate=--plate
+    --plate
     console.log('Средства-',fary,'Тарелок-',plate)
     if (plate==0 && fary>0){
         break;
