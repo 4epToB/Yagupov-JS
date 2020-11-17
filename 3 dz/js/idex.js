46cf853fa7464cd5d1a9b54570119bc819933b5a
4epToB <73242484+4epToB@users.noreply.github.com>
2020-11-17 18:42:58 +0300

    main

--- 3 dz/js/idex.js
+++ 3 dz/js/idex.js
@@ -5,14 +5,13 @@ let C=A.concat(B) //let C=B.concat(A)
 var area = [ 1, null, 0, null, 1, null, null, null, null ]
 let startTable="<table><tbody>";
 let endTable="</tbody></table>";
-let bodyTable="";
-let row="";
 
 for(var i=0;i<area.length;i++){
-    if (area[i]==null){area[i]=""}
-    row = "<tr style='background-color:grey; height:30px'>"+"<td>"+ area[0]+"</td>"+"<td>"+ area[1]+"</td>"+"<td>"+ area[2]+"</td>"+"</tr>" + "<tr style='background-color:grey; height:30px'>"+"<td>"+ area[3]+"</td>"+"<td>"+ area[4]+"</td>"+"<td>"+ area[5]+"</td>"+"</tr>" + "<tr style='background-color:grey; height:30px'>"+"<td>"+ area[6]+"</td>"+"<td>"+ area[7]+"</td>"+"<td>"+ area[8]+"</td>"+"</tr>"
-}
-bodyTable+=row
+    if (area[i]==null){area[i]=""
+    }else if (area[i]==1){area[i]="X"
+    }else if (area[i]==0){area[i]="O" 
+}}
+let bodyTable = "<tr style='background-color:grey; height:30px'>"+"<td>"+ area[0]+"</td>"+"<td>"+ area[1]+"</td>"+"<td>"+ area[2]+"</td>"+"</tr>" + "<tr style='background-color:grey; height:30px'>"+"<td>"+ area[3]+"</td>"+"<td>"+ area[4]+"</td>"+"<td>"+ area[5]+"</td>"+"</tr>" + "<tr style='background-color:grey; height:30px'>"+"<td>"+ area[6]+"</td>"+"<td>"+ area[7]+"</td>"+"<td>"+ area[8]+"</td>"+"</tr>"
 document.write(startTable+bodyTable+endTable)   
 
 
@@ -40,10 +39,43 @@ for(var k=mas.length-1;k>=1;k--){
 mas.shift();
 console.log(mas)
 
-let f="112022"
-let g=f.split('')
-g.forEach(parseint)
-for (r=0;r>g.length-1;r++){
-    if ((g[0]+g[1]+g[2])==(g[3]+g[4]+g[5])){
-        console.log(правда)}
+let count=0;
+for (let c=1001;c<=999999;c++){// раньше чем 001001 счастливых билетов нет,нет смысла искать раньше
+    let f=(c+"").split('') // делаем из числа строку и разбиваем на эл. массива
+    if (f.length==4){ // если число 4х значное,добавляем перед ним два нуля
+        f.unshift(0,0)
+        for (let t=0;t<=5;t++){//каждый эл. массива приводим к числу
+            f[t]=parseInt(f[t])
+            }
+        if((f[0]+f[1]+f[2])==(f[3]+f[4]+f[5])){++count}//проверяем сумму ,прибавляем к счетчику
+    }else if (f.length==5){
+        f.unshift(0)
+        for (let t=0;t<=5;t++){
+            f[t]=parseInt(f[t])
+            }
+        if((f[0]+f[1]+f[2])==(f[3]+f[4]+f[5])){++count}
+    }else if (f.length==6){
+        for (let t=0;t<=5;t++){
+            f[t]=parseInt(f[t])
+            }
+        if((f[0]+f[1]+f[2])==(f[3]+f[4]+f[5])){++count}
+    }
 }
+console.log(count)
+
+
+
+
+let weightsOriginal=[6, 4, 2, 8, 1, 1]
+console.log(weightsOriginal)
+let weights=weightsOriginal.slice()
+weights.sort(function(a, b) {
+    return b - a;
+  })
+console.log(weights)
+/*for (q=weightsOriginal.length;q>=1;q--){
+    for (e=0;e<=weights.length-1;e+2){*/
+    weightsOriginal.splice(weightsOriginal.indexOf(weights[1]),1);
+    console.log(weightsOriginal)
+    weightsOriginal.splice(weightsOriginal.indexOf(weights[1+1],1,(weights[1]-weights[1+1])));
+    console.log(weightsOriginal)
