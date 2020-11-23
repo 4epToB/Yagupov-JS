4806f5b946517ac6a126eaf1fb9dda31fbf3afb8
4epToB <73242484+4epToB@users.noreply.github.com>
2020-11-23 21:45:10 +0300

    main

--- 5 dz/js/idex.js
+++ 5 dz/js/idex.js
@@ -1,17 +1,19 @@
-let mas1 = 'Привет'
-let mas2 = 'Привет'
+let mas1 = ["Привет",]
+let mas2 = ["Привет"]
 function compare(mas1,mas2){
-    if(typeof mas1=='string' && typeof mas2=='string'){
-        if(mas1.length!=mas2.length){return false}
-        else {return true}
+    if(mas1.length==mas2.length){
+       return true
+    }else {return false}
     }
-}
 console.log(compare(mas1,mas2))
 
 function range(start,end,step=1){
     let output=[]
     output[0] = start 
     for (i = 1;; i++){
+        if(step>=end){
+            break;
+            }
         output[i]=output[i-1]+step
         if(output[i]+step>end){
             break;
@@ -19,9 +21,8 @@ function range(start,end,step=1){
     }
     return output
 }
-console.log(range(2,12,3)
-
-)
+console.log(range(2,12,3))
+console.log(range(8,10,2))
 
 
 let student={
@@ -49,10 +50,10 @@ let soglas = "бвгджзйклмнпрстфхцчшщ"
 let keys_glass=glas.split('')
 let keys_soglass=soglas.split('')
 let word = []
-
+let randomLetngth=random(3,5)
 if (random(0,1)==0){
     word[0]=keys_glass[random(0,9)]
-    for(j=1;j<random(3,5);j++){
+    for(j=1;j<randomLetngth;j++){
         if (j%2!=0){word[j]=keys_soglass[random(0,20)]
         } else {
         word[j]=keys_glass[random(0,9)]
@@ -60,7 +61,7 @@ if (random(0,1)==0){
     }
 } else {
     word[0]=keys_soglass[random(0,20)]
-    for(j=1;j<random(3,5);j++){
+    for(j=1;j<randomLetngth;j++){
         if (j%2!=0){word[j]=keys_glass[random(0,9)]
         } else {
         word[j]=keys_soglass[random(0,20)]
